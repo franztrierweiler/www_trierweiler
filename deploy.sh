43d088208a9b04ee7e6fd2d69bdfbad08b823b50
franz <trierweilerfranz@gmail.com>
2019-05-19 10:31:07 +0000

    Ajout fichiers pour démo animejs

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,8 @@ TARGET=/var/www/html
 echo "Déploiement sur Apache"
 cp index.html $TARGET
 cp jstrial.html $TARGET
+cp anime.html $TARGET
 cp -r static $TARGET
+cp -r css $TARGET
+cp -r js $TARGET
 echo "Fin du déploiement"
