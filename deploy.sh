ad31d3398f574d3aefcc1222f46808795d4d75d0
franz <trierweilerfranz@gmail.com>
2020-02-15 15:38:41 +0000

    Ajout de brython

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ echo "Déploiement sur Apache"
 cp index.html $TARGET
 cp jstrial.html $TARGET
 cp anime.html $TARGET
+cp brythontrial.html $TARGET
 cp -r static $TARGET
 cp -r css $TARGET
 cp -r js $TARGET
