592d8b7cfc27c7b1c6a2270b5616649592f5cd47
franz <trierweilerfranz@gmail.com>
2020-03-28 19:43:59 +0000

    Ajout du terminal xterm.js

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ cp index.html $TARGET
 cp jstrial.html $TARGET
 cp anime.html $TARGET
 cp brythontrial.html $TARGET
+cp terminal_sample.html $TARGET
 cp -r static $TARGET
 cp -r css $TARGET
 cp -r js $TARGET
